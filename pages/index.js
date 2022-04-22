20766330d50855b1cf95d25de67ef554a288dcdb
tsuyoshi takeuchi <t-takeuchi@un-t.com>
2022-04-22 18:31:21 +0900

    day.js追加、相対的な日付を取得する機能を追加

--- pages/index.js
+++ pages/index.js
@@ -2,20 +2,26 @@
 import Link from 'next/link';
 import { client } from '../libs/client';
 import { css } from '@emotion/react';
+import dayjs, { locale, extend } from 'dayjs';
+import 'dayjs/locale/ja';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
 export default function Home({ blog }) {
   return (
-    <div>
+    <>
       <ul css={list}>
         {blog.map((blog) => (
           <li key={blog.id} css={list_item}>
             <Link href={`/blog/${blog.id}`}>
               <a>{blog.title}</a>
             </Link>
+            <p>{calcDateDiff(blog.publishedAt)}</p>
           </li>
         ))}
       </ul>
-    </div>
+    </>
   );
 }
 
@@ -28,6 +34,21 @@ export const getStaticProps = async () => {
       blog: data.contents,
     },
   };
+};
+
+//公開日と現在の日付の差分を計算
+const calcDateDiff = (publishedDate) => {
+  locale('ja');
+  extend(utc);
+  extend(timezone);
+  extend(relativeTime);
+
+  const formatedDate = dayjs
+    .utc(publishedDate)
+    .tz('Asia/Tokyo')
+    .format('YYYY-MM-DD');
+  const datediff = dayjs(formatedDate).fromNow();
+  return datediff;
 };
 
 const list = css`
